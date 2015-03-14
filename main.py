5e4997962d1bfc6e0a2484d9b47f7e945303e051
Zihao Wang <wzhdev@gmail.com>
2015-03-14 13:45:33 +0800

    Replace urllib with requests

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python3
 
-import urllib.request
-import urllib.parse
-import http.cookiejar
 import os
+import requests
 
 keypath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'passwd')
 keyfile = open(keypath,'r')
 name, password = keyfile.readline().strip().split(' ')
 port = 7  #出口
 
-#cookie
-cj = http.cookiejar.CookieJar()
-opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 
 #login
-data = urllib.parse.urlencode({'cmd':'login','url':'URL','ip':'','name':name,
-                               'password':password,'go':'登录账户'})
-data = data.encode('utf-8')
-request = urllib.request.Request("http://wlt.ustc.edu.cn/cgi-bin/ip")
+data = dict(cmd='login', url='URL', ip='', name=name, password=password,
+            go='登录账户')
+request_url = "http://wlt.ustc.edu.cn/cgi-bin/ip"
 
-request.add_header("Content-Type","application/x-www-form-urlencoded")
+res = requests.post(request_url, data=data)
 
-f = opener.open(request,data)
-#print(f.read().decode('gb2312'))
-#print(f.info())
 
 #开通网络通
-params = urllib.parse.urlencode({'cmd':'set','url':'URL','type':port,'exe':0,
-                                   'go':'开通网络',})
-#params = params.encode('utf-8')
-res = opener.open("http://wlt.ustc.edu.cn/cgi-bin/ip?%s" % params)
-#print(f.read().decode('gb2312'))
+params = dict(cmd ='set', url='URL', type=port, exe=0, go='开通网络')
+res = requests.post("http://wlt.ustc.edu.cn/cgi-bin/ip?%s" % params)
+
 print("ok")
